f97bcdfc450af9b40831c15b1df58d7259a4989d
留岡佑太郎 <tomeokayutaro@RLSUU103293M01.local>
2017-02-01 17:38:02 +0900

    2016-0201-01

---
+++ Gulp/gulpt/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require(‘gulp’);
+var sass = require("gulp-sass");
+
+
+gulp.task("sass", function() {
+    gulp.src("sass/**/*scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./css"));
+});
+
+gulp.task("default", function() {
+    gulp.watch("sass/**/*.scss",["sass"]);
+});
