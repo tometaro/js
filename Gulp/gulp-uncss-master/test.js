1f2e7e6b894025d3e47e01d277e11cbc94402bbe
留岡佑太郎 <tomeokayutaro@RLSUU103293M01.local>
2017-02-07 18:53:12 +0900

    a

---
+++ Gulp/gulp-uncss-master/test.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var test   = require('tape'),
+    gutil  = require('gulp-util'),
+    uncss  = require('./'),
+    Stream = require('stream'),
+
+    html   = '<html><body><h1>hello</h1></body></html>',
+    css    = 'h2 { color:blue; } h1 { color:red }',
+    output = 'h1 { color:red }';
+
+function fixture (contents) {
+    return new gutil.File({
+        contents: contents,
+        cwd: __dirname,
+        base: __dirname,
+        path: __dirname + '/fixture.css'
+    });
+}
+
+test('should not throw on an empty file', function (t) {
+    t.plan(1);
+
+    var stream = uncss({html: html});
+
+    stream.on('data', function (data) {
+        t.equal(String(data.contents), '');
+    });
+
+    var file = fixture(new Buffer(''));
+
+    stream.write(file);
+});
+
+test('should remove unused css selectors', function (t) {
+    t.plan(1);
+
+    var stream = uncss({html: html});
+
+    stream.on('data', function (data) {
+        t.equal(String(data.contents), output);
+    });
+
+    var file = fixture(new Buffer(css));
+
+    stream.write(file);
+});
+
+test('should throw an error in stream mode', function (t) {
+    t.plan(1);
+
+    var stream = uncss({html: html});
+
+    var file = fixture(new Stream());
+
+    var write = function () {
+        stream.write(file);
+        file.contents.write(css);
+        file.contents.end();
+    };
+
+    t.throws(write, 'should not support streaming contents');
+});
+
+test('should let null files pass through', function (t) {
+    t.plan(1);
+
+    var stream = uncss({html: html});
+
+    stream.on('data', function (data) {
+        t.equal(data.contents, null, 'should not transform null in any way');
+    });
+
+    var file = fixture(null);
+
+    stream.write(file);
+});
--- Gulp/gulpt/gulpfile.js
+++ Gulp/gulpt/gulpfile.js
@@ -1,12 +1,23 @@
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+ 
+// gulp.task('sass', function () {
+//   return gulp.src('./sass/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./css'));
+// });
+ 
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass']);
+// });
+
 var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+var uncss = require('gulp-uncss');
+
+gulp.task('default', function () {
+    return gulp.src('site.css')
+        .pipe(uncss({
+            html: ['index.html', 'posts/**/*.html', 'http://localhost:3000/hoge']
+        }))
+        .pipe(gulp.dest('./out'));
 });
